c9b715bb92de7b15ec5185c27d826aac86c69ca5
Milan Petrovic <milan@gdragon.info>
2020-11-26 21:08:09 +0100

    Few code styling changes.

--- functions.php
+++ functions.php
@@ -1,11 +1,11 @@
 <?php
 
 /** GeneratePress
-  * Load template for aggregated ratings to display after category and tag archive titles.
-  */
-add_action('generate_after_archive_title', 'gdrts__generate_after_archive_title');
+ * Load template for aggregated ratings to display after category and tag archive titles.
+ */
+add_action( 'generate_after_archive_title', 'gdrts__generate_after_archive_title' );
 function gdrts__generate_after_archive_title() {
-	if (is_category() || is_tag()) {
-		require_once('parts/term-aggregated.php');
+	if ( is_category() || is_tag() ) {
+		require_once( 'parts/term-aggregated.php' );
 	}
 }
--- parts/term-aggregated.php
+++ parts/term-aggregated.php
@@ -1,27 +1,27 @@
 <div style="margin-top: 1em; padding-top: 1em; border-top: 1px solid #ddd;">
-	<h4>
-		Aggregated rating for this term
-	</h4>
+    <h4>
+        Aggregated rating for this term
+    </h4>
 	<?php
 
-    /* Get term ID for the current archive page query */
+	/* Get term ID for the current archive page query */
 	$term = get_queried_object_id();
 
 	/* Get aggregated rating of all posts belonging to specified term */
-	$rating = gdrts_aggregate()->posts_by_term($term, array(
-		'post_type'  => 'post',
-		'method'     => 'stars-rating'
-    ));
+	$rating = gdrts_aggregate()->posts_by_term( $term, array(
+		'post_type' => 'post',
+		'method'    => 'stars-rating'
+	) );
 
 	/* Render the custom static stars with the max stars of 5, and specified rating from the aggregated result */
-	echo gdrts_render_custom_stars_block(array(
-	        'stars' => 5,
-            'rating' => $rating->rating,
-            'style_size' => 48
-        ));
+	echo gdrts_render_custom_stars_block( array(
+		'stars'      => 5,
+		'rating'     => $rating->rating,
+		'style_size' => 48
+	) );
 
 	?>
-	<p>
-		Rating: <?php echo $rating->rating; ?>/5, From <?php echo $rating->items; ?> posts and <?php echo $rating->votes; ?> votes.
-	</p>
+    <p>
+        Rating: <?php echo $rating->rating; ?>/5, From <?php echo $rating->items; ?> posts and <?php echo $rating->votes; ?> votes.
+    </p>
 </div>
